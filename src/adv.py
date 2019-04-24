7b163792e2ebb12c5334ed093d708657fab10770
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-04-24 16:48:15 -0400

    added new player that is outside

--- src/adv.py
+++ src/adv.py
@@ -42,6 +42,9 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player(room['outside'])
+
+
 # Write a loop that:
 #
 # * Prints the current room name
